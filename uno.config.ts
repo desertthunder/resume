52a68186daf085997c0858e7f231d1dea569124b
Owais Jamil <desertthunder.dev@gmail.com>
2026-06-09 02:08:26 -0500

    feat: add link icons for accessibility

--- uno.config.ts
+++ uno.config.ts
@@ -1,22 +1,21 @@
 import { defineConfig, presetIcons } from "unocss";
 
-const iconClasses = [
-  "i-ri-user-fill",
-  "i-ri-contacts-book-2-fill",
-  "i-ri-check-fill",
-  "i-ri-briefcase-4-fill",
-  "i-ri-graduation-cap-fill",
-  "i-ri-palette-fill",
-  "i-ri-link",
-  "i-ri-github-fill",
-  "i-ri-sun-fill",
-  "i-ri-moon-fill",
-  "i-ri-linkedin-fill",
-];
-
 export default defineConfig({
   presets: [
     presetIcons({ collections: { ri: () => import("@iconify-json/ri/icons.json").then((module) => module.default) } }),
   ],
-  safelist: iconClasses,
+  safelist: [
+    "i-ri-user-fill",
+    "i-ri-contacts-book-2-fill",
+    "i-ri-check-fill",
+    "i-ri-briefcase-4-fill",
+    "i-ri-graduation-cap-fill",
+    "i-ri-palette-fill",
+    "i-ri-link",
+    "i-ri-github-fill",
+    "i-ri-sun-fill",
+    "i-ri-moon-fill",
+    "i-ri-linkedin-fill",
+    "i-ri-external-link-line",
+  ],
 });
